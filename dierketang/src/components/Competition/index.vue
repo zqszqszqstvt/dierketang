f948153298fa92904fc5bd15dc4b7d4755061efe
zqszqszqstvt <59561423@qq.com>
2023-12-06 17:51:29 +0800

    已完成选择卡片，开始竞赛信息项

--- dierketang/src/components/Competition/index.vue
+++ dierketang/src/components/Competition/index.vue
@@ -1,18 +1,61 @@
 <template>
-  
+  <div>
+    <div class="fl event4-1-detail-box">
+      <h3 class="tit">
+        <em class="common-font">
+          正在报名
+        </em>
+        <em class="title">
+          {{ competition.title }}
+        </em>
+      </h3>
+      <div class="competition-info">
+        <p class="event4-1-plan">
+        <span class="item-tit">主办方</span>{{ competition.organizer }}
+        </p>
+        <p class="event4-1-plan">
+          <span class="item-tit">竞赛级别</span>{{ competition.level }}
+        </p>
+        <p class="event4-1-plan">
+          <span class="item-tit">报名时间</span>{{ competition.registrationTime }}
+        </p>
+        <p class="event4-1-plan">
+          <span class="item-tit">比赛时间</span>{{ competition.competitionTime }}
+        </p>
+      </div>
+    </div>
+
+    <div class="right">
+      <div class="login-button">
+        报名请登录
+      </div>
+      <div class="item-status">
+        距离报名截止还有 {{ competition.remainingDays }} 天
+      </div>
+      <div class="details">
+        <a :href="competition.link" class="common-after-icon v-4-6 event-detail-manage-btn" target="_blank">竞赛详情</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'Competition',
-  components: {
-
-  }
-}
+  data() {
+    return {
+      competition: {
+        title: '【多省市译协盖章】2023年第九届中西部外语翻译大赛',
+        link: 'https://www.saikr.com/vse/cwctic/2023',
+        organizer: '四川省翻译协会 陕西省翻译协会 广西翻译协会 河南省翻译协会 河北省翻译协会 甘肃省翻译工作者协会 宁夏翻译协会 安徽省翻译协会 重庆翻译学会 哈尔滨市翻译协会 山西省翻译协会 贵州省翻译协会 黑龙江省翻译协会 深圳市翻译协会',
+        level: '全国性',
+        registrationTime: '2023.10.21 ～ 2023.12.29',
+        competitionTime: '2023.12.30 ～ 2023.12.31',
+        remainingDays: 23,
+      },
+    };
+  },
+};
 </script>
-
-<style>
-
+<style lang="less" scoped>
+  
 </style>
--- dierketang/src/components/CompetitionChoose/index.vue
+++ dierketang/src/components/CompetitionChoose/index.vue
@@ -1,12 +1,36 @@
 <template>
-  <div class="competition-type-card">
-    <h2>竞赛类型</h2>
-    <div class="competition-types">
-      <div class="type" v-for="type in types" :key="type.id">
-        <h3>{{ type.name }}</h3>
-        <div class="subtypes">
-          <p v-for="subtype in type.subtypes" :key="subtype">{{ subtype }}</p>
+  <div class="box">
+    <div class="category">
+    <!-- 竞赛类别 -->
+    <h3 class="item-title">竞赛类别：</h3>
+    <div v-for="(category, index) in categories" :key="index" class="fl item-box">
+      <h4 class="item-title">
+          <div class="item-link" :class="{ active: category.active }" @click="handleCategoryClick(category.title)">{{ category.title }}</div>
+        </h4>
+        <div class="item-con clearfix" style="display: flex; flex-wrap: wrap;">
+          <div v-for="(subject, sIndex) in category.subjects" :key="sIndex" class="fl item-link" :class="{ active: subject.active }" @click="handleSubjectClick(subject.name)">{{ subject.name }}</div>
+          
         </div>
+    </div>
+  </div>
+
+    <!-- 竞赛级别和排序方式 -->
+    <div class="levelandsort">
+      <div class="item-box clearfix"  style="margin-top: 20px; padding-bottom: 0;">
+        <h3 class="fl item-title">竞赛级别：</h3>
+        <div class="fl item-con clearfix" style="position: relative; top: 6px;">
+          <div v-for="(level, lIndex) in competitionLevels" :key="lIndex" class="item item-link" :class="{ active: level.active }" @click="handleLevelClick(level.name)">{{ level.name }}</div>
+        </div>
+      </div>
+
+      <div class="item-box clearfix" style="margin-top: 20px; padding-bottom: 0;">
+        <h3 class="fl item-title">排序方式：</h3>
+        <div class="fl item-con clearfix" style="position: relative; top: 5px;">
+          <div v-for="(sorting, sIndex) in sortingOptions" :key="sIndex" class="fl item-link" :class="{ active: sorting.active }" @click="handleSortingClick(sorting.label)">{{ sorting.label }}</div>
+        </div>
+        <form action="" class="fr sk-event-screen-form" id="eventScreenForm">
+          <!-- 你的表单内容 -->
+        </form>
       </div>
     </div>
   </div>
@@ -14,26 +38,103 @@
 
 <script>
 export default {
-  name: 'CompetitionChoose',
   data() {
     return {
-      types: [
-        { id: 1, name: '工科', subtypes: ['子类别1', '子类别2'] },
-        { id: 2, name: '文体', subtypes: ['子类别1', '子类别2'] },
-        { id: 3, name: '理科', subtypes: ['子类别1', '子类别2'] },
-        { id: 4, name: '商科', subtypes: ['子类别1', '子类别2'] },
-        { id: 5, name: '综合', subtypes: ['子类别1', '子类别2'] },
-        // 添加更多类型
+      categories: [
+        { title: '全部', active: true},
+        { title: '工科', subjects: [{ name: '数学建模', active: false }, { name: '程序设计', active: false }, { name: '机器人', active: false }, { name: '工程机械', active: false }, { name: '土木建筑', active: false }, { name: '大数据', active: false }, { name: '交通车辆', active: false }, { name: '航天航空', active: false }, { name: '船舶海洋', active: false }, { name: '环境能源', active: false }, { name: '计算机&信息技术', active: false }, { name: '材料高分子', active: false }, { name: '电子&自动化', active: false }], active: false },
+        { title: '文体', subjects: [{ name: '工业&创意设计', active: false }, { name: '外语', active: false }, { name: '演讲主持&辩论', active: false }, { name: '模特', active: false }, { name: '歌舞书画&摄影', active: false }, { name: '体育', active: false }, { name: '科技文化艺术节', active: false }, { name: 'UI设计', active: false }, { name: '服装设计', active: false }, { name: '电子竞技', active: false }], active: false },
+        { title: '理科', subjects: [{ name: '数学', active: false }, { name: '物理', active: false }, { name: '化学化工', active: false }, { name: '健康生命&医学', active: false }, { name: '力学', active: false }], active: false },
+        { title: '商科', subjects: [{ name: '创业', active: false }, { name: '商业', active: false }, { name: '创青春', active: false }], active: false },
+        { title: '综合', subjects: [{ name: '职业技能', active: false }, { name: '挑战杯', active: false }, { name: '环保公益', active: false }, { name: '社会综合', active: false }], active: false }
+        
+        // 添加更多类别
+      ],
+      competitionLevels: [
+        { name: '全部', active: true },
+        { name: '校级', active: false },
+        { name: '市级', active: false },
+        { name: '省级', active: false },
+        { name: '国家级', active: false },
+        { name: '全球级', active: false }
+        // 添加更多级别
+      ],
+      sortingOptions: [
+        { label: '按时间排序', active: true },
+        { label: '按热度排序', active: false }
+        // 添加更多排序方式
       ]
     };
   },
-  components: {
+   methods: {
+    handleCategoryClick(title) {
+      this.categories.forEach((category) => {
+      category.active = category.title === title;
+      if (category.subjects) {
+        category.subjects.forEach((subject) => {
+          subject.active = false;
+        });
+      }
+      });
+    },
+    handleSubjectClick(subjectTitle) {
+      this.categories.forEach((category) => {
+        category.active = false
+        if (category.subjects) {
+          category.subjects.forEach((subject) => {
+            subject.active = subject.name === subjectTitle;
+          });
+        }
+      });
+    },
+    handleLevelClick(title) {
+      this.competitionLevels.forEach((level) => {
+        level.active = level.name === title;
+      });
+    },
 
-  }
-    
-}
+    handleSortingClick(title) {
+      this.sortingOptions.forEach((sorting) => {
+        sorting.active = sorting.label === title;
+      });
+    },
+  },
+};
+
 </script>
-
-<style>
+<style lang="less" scoped>
+.item-title {
+  display: flex;
+  align-items: center;
+  margin: auto 0;
+}
+.item-con {
+  display: flex;
+  flex-wrap: wrap;
+}
+.item-link {
+  display: flex;
+  align-items: center;
+  margin: auto 0;
+  margin-right: 10px; // 右边距
+  margin-bottom: 10px; // 下边距
+  cursor: pointer;
+}
+.item-box {
+  display: flex; 
+  align-items: center;
+  padding: auto;
+}
+.active {
+  color: blue;  // 你可以选择你喜欢的颜色
+}
+.box {
+  border: 1px solid #d8d5d5; /* 设置边框，1px 宽，实线，黑色 */
+  width: 700px; /* 设置宽度为 500px */
+  padding: 15px;
+  border-radius: 10px; /* 设置边框为圆角，半径为 10px */
+  margin-left: 250px; /* 整个组件向右移动 50px */
+  margin-top: 0;
+}
 
 </style>
